305dce295a7bafefaccb2037f1b6a5ddc80a2ffd
Alejandr0salced0 <117870695+Alejandr0salced0@users.noreply.github.com>
2025-02-27 13:43:19 -0500

    Update utils.py

--- utils.py
+++ utils.py
@@ -4,8 +4,4 @@ def createTableIfNotExist(cursor, sentence):
     try:
         cursor.execute(sentence)
     except DatabaseError as e:
-        error, = e.args
-        if error.code == 955:
-            pass
-        else:
-            raise
+        pass
